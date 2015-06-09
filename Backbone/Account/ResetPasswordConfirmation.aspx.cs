f93b99862953f7f7d61a9884a1a8af370cd0de1c
filippo-db <f.diblasi@aifa.gov.it>
2015-06-09 15:29:33 +0200

    prova

---
+++ Backbone/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Backbone.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Backbone/Contact.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Backbone
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+/*
+            SqlConnection conn = new SqlConnection("Server=DDIBLASI0F02077\\SQLEXPRESS;Database=AdventureWorks2014;Trusted_Connection=True;");
+            conn.Open();
+
+            conn.Close();
+*/
+        }
+    }
+}
